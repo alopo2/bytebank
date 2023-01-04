cf8f7409d65dc950272ed2bcb8f573e9224a9737
Angela Carvalho <angelalopo@outlook.com>
2023-01-04 09:14:24 -0300

    adiciona metodos para a classe

--- ContaPoupanca.js
+++ ContaPoupanca.js
@@ -4,4 +4,20 @@ export class ContaPoupanca {
         this._cliente = cliente;
         this._agencia = agencia;
     }
+    sacar(valor) {
+        if (this._saldo >= valor) {
+            this._saldo -= valor;
+            return valor;
+        }
+    }
+
+    depositar(valor) {
+        if (valor <= 0) return;
+        this._saldo += valor;
+    }
+
+    transferir(valor, conta) {
+        const valorSacado = this.sacar(valor);
+        conta.depositar(valorSacado);
+    }
 }
