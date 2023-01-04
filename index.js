216ee3571d3b0cde447a86e463396f00a63aac98
Angela Carvalho <angelalopo@outlook.com>
2023-01-04 09:14:03 -0300

    remove conta2 e instancia nova contaPoupanca

--- index.js
+++ index.js
@@ -1,19 +1,14 @@
 import { Cliente } from "./Cliente.js";
 import { ContaCorrente } from "./ContaCorrente.js";
+import { ContaPoupanca } from "./ContaPoupanca.js";
 
 const cliente1 = new Cliente("Angela", 11122233309);
-const cliente2 = new Cliente("Alice", 88822233309);
-
 
 const contaCorrenteAngela = new ContaCorrente(1001, cliente1);
 contaCorrenteAngela.depositar(500);
-const conta2 = new ContaCorrente(102, cliente2);
+contaCorrenteAngela.sacar(100);
 
+const ContaPoupanca = new ContaPoupanca(50, cliente1, 1001);
 
-let valor = 200;
-contaCorrenteAngela.transferir(valor, conta2);
-
-console.log(conta2)
 console.log(contaCorrenteAngela)
-console.log(ContaCorrente.numeroDeContas)
-
+console.log(ContaPoupanca)
