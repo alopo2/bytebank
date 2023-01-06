b09186d57b6b120629265d933461e42dc328f8a6
Angela Carvalho <angelalopo@outlook.com>
2023-01-06 09:21:19 -0300

    remove os metodos getters and setters da conta

--- ContaCorrente.js
+++ ContaCorrente.js
@@ -3,20 +3,6 @@ import { Cliente } from "./Cliente.js";
 export class ContaCorrente {
     static numeroDeContas = 0;
 
-    set cliente(novoValor) {
-        if (novoValor instanceof Cliente) {
-            this._cliente = novoValor;
-        }
-    }
-
-    get cliente() {
-        return this._cliente;
-    }
-
-    get saldo() {
-        return this._saldo;
-    }
-
     constructor(agencia, cliente) {
         this.agencia = agencia;
         this.cliente = cliente;
